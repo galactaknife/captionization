cef51a6d759fb48ec82066effc9e3f1e0378afff
galactaknife <74742492+galactaknife@users.noreply.github.com>
2021-03-26 10:32:21 -0700

    Add files via upload

--- static/client.js
+++ static/client.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
   $("form").submit(function(e) {
-    // Prevent the form from creating a POST request
+    // Prevent an HTTP POST request
     e.preventDefault();
 
     // Check that the video size isn't larger than 30 MB
-    if ($("input[name='vid']")[0].files[0].size > 30000000) {
+    if ($("input[name='vid']")[0].files[0].size > 50000000) {
       // If file size is too large, notify user
       $("#message").show();
-      $("#message").html("Video file is too large.  Max file size is 30 MB");
+      $("#message").html("Video file is too large.  Max file size is 50 MB");
     }
 
     else {
       // Create FormData instance to send to server
-      var fd = new FormData();
-      fd.append("vid", $("input[name='vid']")[0].files[0]);
-      fd.append("srt", $("input[name='srt']")[0].files[0]);
-      fd.append("extension", $("input[type='file']").get(0).files[0].name.split(".").pop());
+      var f = new FormData();
+
+      // Create file name of output file
+      var name = $("input[type='file']").get(0).files[0].name.split(".")[0] + "-captioned." + $("input[type='file']").get(0).files[0].name.split(".").pop();
+
+      // Add data to form
+      f.append("vid", $("input[name='vid']")[0].files[0]);
+      f.append("srt", $("input[name='srt']")[0].files[0]);
+      f.append("extension", $("input[type='file']").get(0).files[0].name.split(".").pop());
 
       // Notify client that the conversion is starting
       $("#message").show();
@@ -24,14 +29,18 @@ $(document).ready(function() {
       $.ajax({
         url: "/file",
         type: "POST",
-        data: fd,
+        data: f,
         success: function(data) {
           // Show download links
           document.getElementById("output").style.display = "block";
           document.querySelector("#output a").href = data["url"];
+          document.querySelector("#output a:not(a[target='_blank'])").download = name;
 
           // Notify user that the process is complete
           $("#message").html("Captionization process complete!");
+
+          // Reset the form
+          $(this).trigger("reset");
         },
 
         cache: false,
